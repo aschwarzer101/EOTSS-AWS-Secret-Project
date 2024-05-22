00814d235e1e5fa39f2b071183f2624adf98c3d9
aschwarzer101 <124056235+aschwarzer101@users.noreply.github.com>
2024-05-22 14:23:10 -0400

    adding image

--- lib/user-interface/react-app/src/pages/welcome.tsx
+++ lib/user-interface/react-app/src/pages/welcome.tsx
@@ -102,7 +102,7 @@ export default function Welcome() {
                   external: false,
                   type: " ", 
                   href: "/chatbot/playground",
-                  img: "https://images.unsplash.com/photo-1620641788421-7a1c342ea42e?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Z3JhZGllbnR8ZW58MHx8MHx8fDA%3D",
+                  img: "../../public/images/chatbotCard.jpg",
                   description:
                     "Experiment using different models to automate your everyday tasks",
                 },

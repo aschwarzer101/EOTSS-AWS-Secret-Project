fe7951f04aaea855c4097bb8fb4dc91f8b5d0508
Dhruv Kamalesh Kumar <dhruvbaradiya@gmail.com>
2024-05-25 20:31:14 -0400

    new image for multichat

--- lib/user-interface/react-app/src/pages/welcome.tsx
+++ lib/user-interface/react-app/src/pages/welcome.tsx
@@ -118,7 +118,8 @@ export default function Welcome() {
                                             external: false,
                                             type: " ",
                                             href: "/chatbot/multichat",
-                                            img: "https://images.unsplash.com/photo-1579546928937-641f7ac9bced?q=80&w=1878&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                                            img: "/images/welcome/multichat.png",
+                                            //img: "https://images.unsplash.com/photo-1579546928937-641f7ac9bced?q=80&w=1878&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
                                             description:
                                                 "Compare how models respond to the same prompt",
                                         },
